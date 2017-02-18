7c7927e11f8ca54265ccd502c67831ab39e1c148
Derwin <derwinsadiwa@gmail.com>
2017-02-18 15:16:53 +0800

    modified content

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,26 @@
 var gulp            = require('gulp'),
     browserSync     = require('browser-sync').create(),
+    pug             = require('gulp-pug'),
     autoprefixer    = require('gulp-autoprefixer'),
     sass            = require('gulp-sass'),
     sourcemaps      = require('gulp-sourcemaps'),
     babel           = require("gulp-babel");
 
+/* Pug Transpiler */
+
+gulp.task('pug', function () {
+  return gulp.src('./src/pug/*.pug')
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.stream());
+});
+
 /* SASS Transpiler */
 
 gulp.task('sass', function () {
-  return gulp.src('./app/sass/*.scss')
+  return gulp.src('./src/sass/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass({outputStyle: 'nested'}).on('error', sass.logError))
     .pipe(autoprefixer())
@@ -20,7 +32,7 @@ gulp.task('sass', function () {
 /* ES6 Transpiler */
 
 gulp.task('es6', function () {
-  return gulp.src('./app/es6/main.js')
+  return gulp.src('./src/es6/main.js')
     .pipe(sourcemaps.init())
     .pipe(babel())
     .pipe(sourcemaps.write('./'))
@@ -30,14 +42,15 @@ gulp.task('es6', function () {
 
 /* Static Server BrowserSync */
 
-gulp.task('serve', ['sass', 'es6'], function() {
+gulp.task('serve', ['pug', 'sass', 'es6'], function() {
 
     browserSync.init({
       server: './dist'
     });
 
-    gulp.watch('./app/sass/**/*.scss', ['sass']);
-    gulp.watch('./app/es6/*.js', ['es6']);
+    gulp.watch('./src/**/*.pug', ['pug']);
+    gulp.watch('./src/sass/**/*.scss', ['sass']);
+    gulp.watch('./src/es6/*.js', ['es6']);
 
     gulp.watch('./dist/*.html').on('change', browserSync.reload);
 });
